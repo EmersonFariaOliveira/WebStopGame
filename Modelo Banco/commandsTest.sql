ba5953bc493301e13c94b4a82db9adb82f93b27e
Jefferson Cleyson Gomes Almeida <jefferson.almeida@inatel.br>
2018-10-02 11:13:03 -0300

    Alter model sql

--- Modelo Banco/commandsTest.sql
+++ Modelo Banco/commandsTest.sql
@@ -1,2 +1,6 @@
-insert into user values(1, "jefferson","jefferson@gmail.com", "inatelsemfio");
+insert into perfil values(1, 0, 0, 0);
+insert into user values(1, "jefferson","jefferson@gmail.com", "inatelsemfio", 1);
+
 select *from user where nome = "jefferson";
+select *from perfil;
+
